bb14b9356ac10b6eaf7c1e1f731f59a1d3b3aff3
Jairo Guzmán <jjguz17@gmail.com>
2018-11-13 14:02:19 -0600

    Update /conf/db.conf
    Cambio de BD local por BD en db4free.net

--- conf/db.conf.php
+++ conf/db.conf.php
@@ -1,13 +1,13 @@
 <?php
     define("GESTOR_DB","mysql");//definimos el servidor de base de datos que vamos a utilizar
     
-    define("SERVIDOR_BD","localhost");//definimos la direccion del servidor en la cual nos conectaremos a la BD
+    define("SERVIDOR_BD","db4free.net");//definimos la direccion del servidor en la cual nos conectaremos a la BD
 
-    define("USUARIO_BD","root");//el usuario que utilizaremos para conectarnos a la BD
+    define("USUARIO_BD","tekhubuser");//el usuario que utilizaremos para conectarnos a la BD
 
-    define("PASS_BD","");//la contraseña del usuario de la base de datos
+    define("PASS_BD","15b771b4");//la contraseña del usuario de la base de datos
 
-    define("DB_BD","ADB");//BD en el cual nos conectaremos
+    define("DB_BD","revatdb");//BD en el cual nos conectaremos
 
     define("PORT_DB",3306);//Puerto en el cual nos conectaremos   
 ?>
